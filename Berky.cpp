d30a3a05d597fb025e44da9e7bdd535c6d746b20
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-06-03 16:38:24 +0530

    Create Berky.cpp

---
+++ Berky.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class BerkeyG{
+      public:
+          void printHappy(){cout<<"Hai I am very happy";}
+
+
+};
+int main()
+{
+    BerkeyG berk;
+    berk.printHappy();
+    return 0;
+}
